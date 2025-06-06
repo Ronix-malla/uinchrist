69aa0d703fd529ccecc3948ac2199a101c436083
Ronix-malla <ronixmalla21@gmail.com>
2025-06-06 17:41:03 +0545

    changes

--- logicDomain/app.js
+++ logicDomain/app.js
@@ -76,3 +76,41 @@ window.addEventListener('scroll', () => {
     topButton.classList.remove('active');
   }
 });
+
+
+
+// content switiching inner section
+
+document.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".tab");
+  const tabContents = document.querySelectorAll(".tab-content");
+
+  function resetAnimation(element, className) {
+    element.classList.remove(className);
+
+    void element.offsetWidth;
+
+    element.classList.add(className);
+  }
+
+  tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+      const targetId = tab.getAttribute("data-tab");
+
+      tabs.forEach(t => t.classList.remove("active"));
+      tabContents.forEach(c => c.classList.remove("active"));
+
+      tab.classList.add("active");
+      const activeContent = document.getElementById(targetId);
+      activeContent.classList.add("active");
+
+      activeContent.querySelectorAll(".toogle-left").forEach(el => {
+        resetAnimation(el, "toogle-left-show");
+      });
+
+      activeContent.querySelectorAll(".toogle-right").forEach(el => {
+        resetAnimation(el, "toogle-right-show");
+      });
+    });
+  });
+});
